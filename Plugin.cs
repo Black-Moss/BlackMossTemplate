af27ac01ffdb4842fabdeeb5d08d7e6e82a1f369
Black_Moss <kytheminecraft@outlook.com>
2026-01-23 20:12:37 +0800

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using MonoMod.RuntimeDetour;
+using UnityEngine;
+
+namespace ModNamespace
+{
+    [BepInPlugin("blackmoss.template", "BlackMossTemplate", "0.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static new ManualLogSource Logger;
+        private readonly Harmony _harmony = new("blackmoss.template");
+        public static Plugin Instance { get; private set; } = null!;
+
+        public void Awake()
+        {
+            Logger = base.Logger;
+            Instance = this;
+
+            _harmony.PatchAll();
+            Logger.LogInfo("Here's Black Moss!");
+        }
+    }
+}
