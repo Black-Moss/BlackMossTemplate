b467e6d68cc373dff54c06487aff5f94f827c4fb
Black_Moss <kytheminecraft@outlook.com>
2026-02-08 19:43:56 +0800

    new

--- obj/Debug/net472/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace BlackMossTemplate
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "BlackMossTemplate";
-        public const string PLUGIN_NAME = "BlackMossTemplate";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
